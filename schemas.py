b013cfc451eff5a8341daee930016ef8fe7d78c1
isidro <isidro107109@gmail.com>
2026-07-15 17:26:24 +0100

    schemas.py terminado

--- schemas.py
+++ schemas.py
@@ -13,5 +13,12 @@ class LibroResponse(BaseModel):
     autor: str
     precio: float
     disponible: bool
-
+    
+class PeliculaCreate(BaseModel):
+    titulo: str = Field(min_length=1, max_length=100)
+    director: str = Field(min_length=3, max_length=50)
+    año: int = Field(ge=1895, le=2026)
+    duracion_minutos: int = Field(gt=0 ,le=360)
+    activa: bool = True
+    
     model_config = {"from_attributes": True}
