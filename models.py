6ca028bdadba5db71911e4192355a2d408aaa3ff
isidro <isidro107109@gmail.com>
2026-07-17 21:25:25 +0100

    se agrego reseña a models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
 from database import Base
+from sqlalchemy.orm import relationship
 
 class Libro(Base):
     __tablename__ = "libros"
@@ -20,3 +21,15 @@ class Pelicula(Base):
     año = Column(Integer, nullable=False)
     duracion_minutos = Column(Integer, nullable=False)
     activa = Column(Boolean, default=True)
+    
+    reseñas = relationship("Reseñas", back_populates="pelicula")
+    
+class Reseña(Base):
+    __tablename__ = "reseñas"
+    id = Column(Integer, primary_key=True, index=True)
+    texto = Column(String(500), nullable=False)
+    puntuacion = Column(Integer, nullable=False)
+    
+    pelicula_id = Column(Integer, ForeignKey("peliculas.id"), nullable=False)
+    
+    pelicula = relationship("Pelicula", back_populates="reseñas")
